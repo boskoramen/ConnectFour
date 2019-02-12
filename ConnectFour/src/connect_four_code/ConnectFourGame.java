8ec7937f62f90b703f16782a939b29cb6ba2217f
boskoramen <isaiah@localhost.localdomain>
2019-02-12 16:47:28 -0500

    Reformatted ConnectFourGame class to make it more readable so that I can actually refactor it later (lol). Still a lot more to do style-wise but this is a start.

--- ConnectFour/src/connect_four_code/ConnectFourGame.java
+++ ConnectFour/src/connect_four_code/ConnectFourGame.java
@@ -1,5 +1,4 @@
 /*
- *
  * @author isaiah.cruz
  */
 
@@ -18,24 +17,27 @@ import javax.swing.JOptionPane;
 import game_framework.*;
 
 public class ConnectFourGame extends Thread implements ActionListener{
-    
-	private ArrayList<Integer> moves;
-	private Board board;
-	private ConnectFourGUI GUI;
-	final File file = new File(ConnectFourGUI.class.getProtectionDomain().getCodeSource().getLocation().getPath());   
-	private final boolean DEBUG = false;
-	private String recordingFilename = ""; // To be used in the future for recording (and possibly replaying) steps in a game previously played
-	private boolean isFirstPlayerTurn;
-	private boolean finished;
-	final boolean testPhase;
-	private boolean stop = false;
-	private static Tree<Move> memory;
-	private final ArrayList<Player> humanPlayers;
+
+    //Private instance variables:
+    private ArrayList<Integer> moves;
+    private Board board;
+    private ConnectFourGUI GUI;
+
+    private final boolean DEBUG = false;
+    private String recordingFilename = ""; // To be used in the future for recording (and possibly replaying) steps in a game previously played
+    private boolean isFirstPlayerTurn;
+    private boolean finished;
+    final boolean testPhase;
+    private boolean stop = false;
+    private static Tree<Move> memory;
+    private final ArrayList<Player> humanPlayers;
     private final ArrayList<ConnectFourMachine> AIPlayers;
     private final ArrayList<Player> curPlayerPool;
     private int curPlayerPos;
-	private final Timer AITimer;
-	public ConnectFourGame(boolean isTest, Tree<Move> a_memory) {
+    private final Timer AITimer;
+
+
+    public ConnectFourGame(boolean isTest, Tree<Move> a_memory) {
         testPhase = isTest;
         memory = a_memory;
         board = new Board();
@@ -43,7 +45,7 @@ public class ConnectFourGame extends Thread implements ActionListener{
         humanPlayers = new ArrayList<Player>();
         AIPlayers = new ArrayList<ConnectFourMachine>();
         curPlayerPool = new ArrayList<Player>();
-	    AITimer = new Timer(0, this);
+	AITimer = new Timer(0, this);
         Player firstPlayer = new Player();
         Player secondPlayer = new Player();
         firstPlayer.setIdentifier("name", "red");
@@ -52,231 +54,227 @@ public class ConnectFourGame extends Thread implements ActionListener{
         secondPlayer.setIdentifier("color", Color.black);
         humanPlayers.add(firstPlayer);
         humanPlayers.add(secondPlayer);
-	    GUI = new ConnectFourGUI(board, this);
+	GUI = new ConnectFourGUI(board, this);
         ConnectFourMachine alphaPlayer = new ConnectFourMachine(board, isFirstPlayerTurn);
-	    ConnectFourMachine betaPlayer = new ConnectFourMachine(board, !isFirstPlayerTurn);
+	ConnectFourMachine betaPlayer = new ConnectFourMachine(board, !isFirstPlayerTurn);
         if(testPhase) {
             curPlayerPool.add(alphaPlayer);
             curPlayerPool.add(betaPlayer);
-        }
-        else {
-        	curPlayerPool.add(firstPlayer);
-        	curPlayerPool.add(secondPlayer);
+        } else {
+            curPlayerPool.add(firstPlayer);
+            curPlayerPool.add(secondPlayer);
         }
         curPlayerPos = 0;
-	    startGame();
-	    GUI.repaint();
-	}
+	startGame();
+	GUI.repaint();
+    }
 	
-	public Board getBoard() {
-		return board;
-	}
+    public Board getBoard() {
+	return board;
+    }
 	
-	public Player currentPlayer() {
-		return curPlayerPool.get(0);
-	}
+    public Player currentPlayer() {
+	return curPlayerPool.get(0);
+    }
 	
-	public Tree<Move> getMemory() {
-		return memory;
-	}
+    public Tree<Move> getMemory() {
+       	return memory;
+    }
 	
-	public boolean isFinished() {
-		return finished;
-	}
+    public boolean isFinished() {
+        return finished;
+    }
 	
-	public boolean isFirstPlayerTurn() {
-		return isFirstPlayerTurn;
-	}
+    public boolean isFirstPlayerTurn() {
+	return isFirstPlayerTurn;
+    }
 	
-	// Implemented, but not used at the moment
-	public void getCode() {
-		recordingFilename = "ConnectFour-"
-	    					+  randomWithRange(1, 9) 
-	    					+ randomWithRange(0, 9)
-	    					+ randomWithRange(0, 9)
-	    					+ getRandomLetter() 
-	    					+ getRandomLetter() 
-	    					+ getRandomLetter() 
-	    					+ getRandomLetter() 
-	    					+ getRandomLetter()
-	    					+ ".con";
-	}
+    // Implemented, but not used at the moment
+    public void getCode() {
+	recordingFilename = "ConnectFour-"
+	    + randomWithRange(1, 9) 
+	    + randomWithRange(0, 9)
+	    + randomWithRange(0, 9)
+	    + getRandomLetter() 
+	    + getRandomLetter() 
+	    + getRandomLetter() 
+	    + getRandomLetter() 
+	    + getRandomLetter()
+	    + ".con";
+    }
 	
-	// Utilized by getCode()
-	public String getRandomLetter() {
-		int randomNum = randomWithRange(0, 25);
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-	    return alphabet.substring(randomNum, randomNum + 1);
-	}
+    // Utilized by getCode()
+    public String getRandomLetter() {
+	int randomNum = randomWithRange(0, 25);
+	String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        return alphabet.substring(randomNum, randomNum + 1);
+    }
 	
-	public int randomWithRange(int min, int max) {
-		int range = (max - min) + 1;     
-	    return (int)(Math.random() * range) + min;
-	}
+    public int randomWithRange(int min, int max) {
+	int range = (max - min) + 1;     
+        return (int)(Math.random() * range) + min;
+    }
 	
-	public void printMoves() {
-		int len = moves.size();
-	    int k;
-	    
-	    try {
-	    	File dir = new File("moves");
-	    	dir.mkdirs();
-	    	FileWriter writer = new FileWriter(new File(dir, recordingFilename));
-	        writer.write(isFirstPlayerTurn + "\n");
-	    	for (k = 0; k < len; k++) {
-	    		writer.write(moves.get(k) + "\n");
-	        }
-	        writer.close();
-		} 
-	    catch (IOException exception) {
-			System.out.println("Error processing file: " + exception);
+    public void printMoves() {
+        int len = moves.size();
+	int k;
+	
+	try {
+	    File dir = new File("moves");
+	    dir.mkdirs();
+	    FileWriter writer = new FileWriter(new File(dir, recordingFilename));
+	    writer.write(isFirstPlayerTurn + "\n");
+	    for (k = 0; k < len; k++) {
+	    writer.write(moves.get(k) + "\n");
 	    }
+
+	    writer.close();
+	} catch (IOException exception) {
+	    System.out.println("Error processing file: " + exception);
 	}
+    }
 	
-	public void drop(int xpos, boolean isFirstPlayer) {
-		System.out.println(testPhase);
-		boolean validDrop = board.addPiece(xpos, isFirstPlayer);
-		boolean outcome = false;
-		if(!validDrop) {
-        	if(!checkAITurn(alphaPlayer) && !checkAITurn(betaPlayer)) {
-        		JOptionPane.showMessageDialog(null, "There is no more space in this column!", "Hey!", JOptionPane.ERROR_MESSAGE);
-        	}
-    	}
-        else {
-        	moves.add(xpos);
+    public void drop(int xpos, boolean isFirstPlayer) {
+	System.out.println(testPhase);
+	boolean validDrop = board.addPiece(xpos, isFirstPlayer);
+	boolean outcome = false;
+        if(!validDrop) {
+            if(!checkAITurn(alphaPlayer) && !checkAITurn(betaPlayer)) {
+        	JOptionPane.showMessageDialog(null, "There is no more space in this column!", "Hey!", JOptionPane.ERROR_MESSAGE);
+            }
+    	} else {
+            moves.add(xpos);
             GUI.repaint(true);
             outcome = board.checkWinner(isFirstPlayer);
-            
-			if(outcome) {
-	        	String msg = "";
-	            if(isFirstPlayerTurn) {
-	            	if(firstPlayer.getIdentifier("name").equals(GUI.colorToString((Color) firstPlayer.getIdentifier("color")))) {
-	                    msg += "The "+ firstPlayer.getIdentifier("name") +" player won";
-	            	}
-	                else {
-	                    msg += "The player "+ firstPlayer.getIdentifier("name") +" won";
-	                }
+
+	    if(outcome) {
+                String msg = "";
+	        if(isFirstPlayerTurn) {
+	            if(firstPlayer.getIdentifier("name").equals(GUI.colorToString((Color) firstPlayer.getIdentifier("color")))) {
+	                msg += "The "+ firstPlayer.getIdentifier("name") +" player won";
+	            } else {
+	                msg += "The player "+ firstPlayer.getIdentifier("name") +" won";
 	            }
-	            else {
-	            	if(secondPlayer.getIdentifier("name").equals(GUI.colorToString((Color) secondPlayer.getIdentifier("color")))) {
-	                    msg += "The "+ secondPlayer.getIdentifier("name") +" player won";
-	            	}
-	                else {
-	                    msg += "The player "+ secondPlayer.getIdentifier("name") +" won";
-	                }
-	            }
-	            if(!testPhase) {
-	            	JOptionPane.showMessageDialog(null, msg, "We have a winner!", JOptionPane.INFORMATION_MESSAGE);
-	            }
-	            finished = true;
-	            if(DEBUG) {
-	            	printMoves();
+	        } else {
+	            if(secondPlayer.getIdentifier("name").equals(GUI.colorToString((Color) secondPlayer.getIdentifier("color")))) {
+	                msg += "The "+ secondPlayer.getIdentifier("name") +" player won";
+	            } else {
+	                msg += "The player "+ secondPlayer.getIdentifier("name") +" won";
 	            }
 	        }
-	        else if(board.fullBoardCheck()) {
-	        	String msg = "There is no more space left on the board!";
-	        	if(!checkAITurn(alphaPlayer) && !checkAITurn(betaPlayer)) {
-	        		JOptionPane.showMessageDialog(null, msg, "It's a draw!", JOptionPane.INFORMATION_MESSAGE);
-	        	}
-	            finished = true;
-	            if(DEBUG) {
-	            	printMoves();
-	            }
-	        }   
-	        if(!finished) {
-	        	if(isFirstPlayerTurn) isFirstPlayerTurn = false;
-	            else isFirstPlayerTurn = true;
+
+		if(!testPhase) {
+	            JOptionPane.showMessageDialog(null, msg, "We have a winner!", JOptionPane.INFORMATION_MESSAGE);
 	        }
+
+	        finished = true;
+	        if(DEBUG) {
+	            printMoves();
+	        }
+	    } else if(board.fullBoardCheck()) {
+	        String msg = "There is no more space left on the board!";
+	        if(!checkAITurn(alphaPlayer) && !checkAITurn(betaPlayer)) {
+	            JOptionPane.showMessageDialog(null, msg, "It's a draw!", JOptionPane.INFORMATION_MESSAGE);
+	        }
+
+		finished = true;
+	        if(DEBUG) {
+	            printMoves();
+	        }
+	    }   
+
+	    if(!finished) {
+	        if(isFirstPlayerTurn) {
+		    isFirstPlayerTurn = false;
+		} else {
+		    isFirstPlayerTurn = true;
+		}
+	    }
         }
 		
         if(alphaPlayer.isActivated()) {
-    		alphaPlayer.processMove(isFirstPlayerTurn, validDrop, outcome);
+    	    alphaPlayer.processMove(isFirstPlayerTurn, validDrop, outcome);
     	}
     	if(betaPlayer.isActivated()) {
-    		betaPlayer.processMove(isFirstPlayerTurn, validDrop, outcome);
+    	    betaPlayer.processMove(isFirstPlayerTurn, validDrop, outcome);
     	}
+	
     	GUI.repaint();
-	}
+    }
 	
-	public boolean checkAIActivated() {
-		if(alphaPlayer.isActivated() || betaPlayer.isActivated()) {
-	    	return true;
-	    }
-	    else {
-	    	return false;
-	    }
+    public boolean checkAIActivated() {
+	if(alphaPlayer.isActivated() || betaPlayer.isActivated()) {
+    	    return true;
+	} else {
+	    return false;
 	}
+    }
 	
-	public boolean checkAITurn(ConnectFourMachine AI) {
-		if(AI.isCurrentPlayer(isFirstPlayerTurn) && AI.isActivated() && !finished) {
-	    	return true;
-	    }
-	    else {
-	    	return false;
-	    }
+    public boolean checkAITurn(ConnectFourMachine AI) {
+        if(AI.isCurrentPlayer(isFirstPlayerTurn) && AI.isActivated() && !finished) {
+	    return true;
+	} else {
+	    return false;
 	}
+    }
 	
-	public boolean checkAITurn() {
-		if(checkAITurn(alphaPlayer) || checkAITurn(betaPlayer)) {
-	    	return true;
-	    }
-	    else {
-	    	return false;
-	    }
+    public boolean checkAITurn() {
+        if(checkAITurn(alphaPlayer) || checkAITurn(betaPlayer)) {
+	    return true;
+	} else {
+	    return false;
 	}
+    }
 	
-	public void playAITurn(ConnectFourMachine AI) {
-		AI.nextNode(!testPhase, isFirstPlayerTurn);
-		drop(AI.getCurrentNode().getValue().getPosition(), isFirstPlayerTurn);
-		GUI.repaint();
+    public void playAITurn(ConnectFourMachine AI) {
+        AI.nextNode(!testPhase, isFirstPlayerTurn);
+        drop(AI.getCurrentNode().getValue().getPosition(), isFirstPlayerTurn);
+        GUI.repaint();
+    }
+	
+    public void startGame() {
+        if(randomWithRange(0, 1) == 0) {
+	    isFirstPlayerTurn = true;
+	} else {
+	    isFirstPlayerTurn = false;
 	}
-	
-	public void startGame() {
-		if(randomWithRange(0, 1) == 0) {
-			isFirstPlayerTurn = true;
-		}
-	    else {
-	    	isFirstPlayerTurn = false;
-		}
-	    finished = false;
-	    board.clearBoard();
-	    getCode();
-	    moves.clear();
-	    if(alphaPlayer.isActivated()) {
-	    	alphaPlayer.newGame(isFirstPlayerTurn, memory);
-	    }
-	    if(betaPlayer.isActivated()) {
-	    	betaPlayer.newGame(isFirstPlayerTurn, memory);
-	    }
+
+	finished = false;
+	board.clearBoard();
+        getCode();
+	moves.clear();
+	if(alphaPlayer.isActivated()) {
+	    alphaPlayer.newGame(isFirstPlayerTurn, memory);
+	}
+	if(betaPlayer.isActivated()) {
+	    betaPlayer.newGame(isFirstPlayerTurn, memory);
+	}
 	    
-	    if(betaPlayer.isActivated() && alphaPlayer.isActivated()) {
-	    	AITimer.start();
-	    }
+	if(betaPlayer.isActivated() && alphaPlayer.isActivated()) {
+	    AITimer.start();
+	}
 	    GUI.repaint();
-	}
+    }
+    
+    public void terminate() {
+        stop = true;
+        this.interrupt();
+    }
 	
-	public void terminate() {
-		stop = true;
-                this.interrupt();
-	}
-	
-	// Respond to a button click in the game
-	public void actionPerformed(ActionEvent e) {
-	    if(!stop) {
-			if(checkAITurn(alphaPlayer) && !GUI.isAnimating()) {
-	    		playAITurn(alphaPlayer);
-	    		GUI.repaint();
-	    	}
-	    	else if(checkAITurn(betaPlayer) && !GUI.isAnimating()) {
-	    		playAITurn(betaPlayer);
-	    		GUI.repaint();
-	    	}
-	    	else if(finished && testPhase) {
-	    		startGame();
-	    	}
-	    	else if(!testPhase) {
-	    		GUI.setVisible(true);
-	    	}
+    // Respond to a button click in the game
+    public void actionPerformed(ActionEvent e) {
+        if(!stop) {
+	    if(checkAITurn(alphaPlayer) && !GUI.isAnimating()) {
+	        playAITurn(alphaPlayer);
+	    	GUI.repaint();
+	    } else if(checkAITurn(betaPlayer) && !GUI.isAnimating()) {
+	        playAITurn(betaPlayer);
+	        GUI.repaint();
+	    } else if(finished && testPhase) {
+	        startGame();
+	    } else if(!testPhase) {
+	        GUI.setVisible(true);
 	    }
 	}
+    }
 }
